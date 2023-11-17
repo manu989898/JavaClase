34d03fb2999b668eaefb7c1a5925b7db6157a3b7
manu989898 <apkmanu1@gmail.com>
2023-11-17 17:40:44 +0100

    alfiiiin

--- Tema3/src/Actividades/Ejer14d.java
+++ Tema3/src/Actividades/Ejer14d.java
@@ -8,42 +8,43 @@ public class Ejer14d {
          * primos. Recuerda que un número entero es primo si no puede dividirse por
          * ninguno que no sea 1 o él mismo.
          * Los primeros números primos son: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, ...u
-        */
-        int num = 2;
-        boolean esPrrimo=true;
+         */
+        int num = 3;
+        boolean esPrrimo = true;
         int[] numeros = new int[100];
         int posicion = 0;
-       
+        int i = 2;
+
         while (true) {
-            
-            for (int i = 2; i <= num - 1; i++) {
+
+            for (i = 2; i <= (num - 1); i++) {
 
                 if (num % i == 0) {
 
                     esPrrimo = false;
 
                 }
-
             }
 
-            if (esPrrimo=true) {
+            if (esPrrimo) {
 
                 numeros[posicion] = num;
                 posicion++;
 
             }
 
-            if (posicion > 99) {
+            if (posicion == 100) {
                 break;
             }
-
+            // System.out.println(num + " " + esPrrimo);
+            
             num++;
+            esPrrimo = true;
+            i = 2;
 
         }
-
         for (int k = 0; k < numeros.length; k++) {
-            System.out.println(numeros[k]);
+            System.out.println("Posición --> ["+ k + "]" + "  Número primo: " + numeros[k]);
         }
-
     }
 }
