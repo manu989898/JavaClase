87f3592a22a35de6c66b15b7990386326f8e2c66
manu989898 <apkmanu1@gmail.com>
2023-11-17 19:21:43 +0100

    m

--- Tema3/src/Actividades/Ejer14d.java
+++ Tema3/src/Actividades/Ejer14d.java
@@ -9,7 +9,7 @@ public class Ejer14d {
          * ninguno que no sea 1 o él mismo.
          * Los primeros números primos son: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, ...u
          */
-        int num = 3;
+        int num = 2;
         boolean esPrrimo = true;
         int[] numeros = new int[100];
         int posicion = 0;
@@ -25,27 +25,25 @@ public class Ejer14d {
 
                 }
             }
-
             if (esPrrimo) {
 
                 numeros[posicion] = num;
                 posicion++;
 
             }
-
             if (posicion == 100) {
                 break;
             }
             // System.out.println(num + " " + esPrrimo);
-            
-            //Reinicios y actualizaciones de variables/contadores
+
+            // Reinicios y actualizaciones de variables/contadores
             num++;
             esPrrimo = true;
             i = 2;
 
         }
         for (int k = 0; k < numeros.length; k++) {
-            System.out.println("Posición --> ["+ k + "]" + "  Número primo: " + numeros[k]);
+            System.out.println("Posición --> [" + k + "]" + "  Número primo: " + numeros[k]);
         }
     }
 }
