29c08428a2e9e6df8f312a37dcbd975693e3c9be
manu989898 <apkmanu1@gmail.com>
2023-11-17 16:59:45 +0100

    m

--- Tema3/src/Actividades/Ejer14d.java
+++ Tema3/src/Actividades/Ejer14d.java
@@ -2,18 +2,47 @@ package Actividades;
 
 public class Ejer14d {
     public static void main(String[] args) {
-        /*programa que pida un número y dice si es primo o no.num-1 y 2*/
+        /*
+         * Escribe un programa que rellene un array llamado primos con los 100 primeros
+         * números
+         * primos. Recuerda que un número entero es primo si no puede dividirse por
+         * ninguno que no sea 1 o él mismo.
+         * Los primeros números primos son: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, ...u
+        */
         int num = 2;
         boolean esPrrimo=true;
+        int[] numeros = new int[100];
+        int posicion = 0;
+       
+        while (true) {
+            
+            for (int i = 2; i <= num - 1; i++) {
 
-        for (int i = 2; i<=num-1 ; i++){
+                if (num % i == 0) {
 
-            if (num%i==0) {
+                    esPrrimo = false;
 
-                esPrrimo = false;
-                System.out.println("false");
-            } 
+                }
 
+            }
+
+            if (esPrrimo=true) {
+
+                numeros[posicion] = num;
+                posicion++;
+
+            }
+
+            if (posicion > 99) {
+                break;
+            }
+
+            num++;
+
+        }
+
+        for (int k = 0; k < numeros.length; k++) {
+            System.out.println(numeros[k]);
         }
 
     }
---
+++ Tema3/src/Ejemplos/BusquedaArrayDesordenado.java
@@ -0,0 +1,45 @@
+package Ejemplos;
+
+import java.util.Scanner;
+
+public class BusquedaArrayDesordenado {
+    public static void main(String[] args) {
+        
+        /*
+         * 
+         */
+
+        Scanner escaner = new Scanner (System.in);
+
+        double[] tallas = {37, 40, 40.5, 39, 45, 44, 43.5, 48, 42.5, 41.5};
+        boolean encontrado = false;
+        int i = 0;
+        //Leemos una talla desde la terminal
+        System.out.println("Introduce la talla que deseas buscar");
+
+        double talla = escaner.nextDouble();
+
+        //Buscamos si la talla se encuentra en el array
+        for (i = 0; i< tallas.length; i++) {
+
+            if (tallas[i] == talla) {
+
+                encontrado=true;
+                break;
+
+            } 
+
+        }
+
+        if (encontrado = false){
+
+            System.out.println("No encontramos la talla");
+
+        } else {
+
+            System.out.println("Tenemos la talla en stock");
+
+        }
+        escaner.close();
+    }
+}
