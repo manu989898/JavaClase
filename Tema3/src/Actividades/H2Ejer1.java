e5591967abc66257ed1c72a1540c204ce5c1af6d
Manuel <143644838+manu989898@users.noreply.github.com>
2023-11-19 14:23:56 +0100

    Add files via upload

---
+++ Tema3/src/Actividades/H2Ejer1.java
@@ -0,0 +1,45 @@
+package Actividades;
+
+import java.util.Scanner;
+
+public class H2Ejer1 {
+    public static void main(String[] args) {
+
+        /*
+         * Crea un programa que guarde la siguiente tabla de edades en forma de array. A
+         * continuación
+         * deberá buscar si hay alguna persona con 35 años. Si la hay, se mostrará el
+         * mensaje "Localizada
+         * persona de 35 años". Utiliza una variable semáforo llamada edadEncontrada.
+         * 20 25 30 28 39 42 30
+         * 29 28 21 60 55 20 19
+         */
+
+        Scanner escaner = new Scanner(System.in);
+
+        int[] edades = {20, 25, 30, 28, 39, 42, 30, 29, 28, 21, 60, 55, 20, 19};
+        boolean edadEncontrada=false;
+        int buscada;
+
+        System.out.println("Introduce la edad a buscar");
+        buscada = escaner.nextInt();
+
+        for (int i = 0; i < edades.length; i++) {
+
+            if (edades[i] == buscada) {
+
+                edadEncontrada = true;
+            }
+
+        }
+
+        if (edadEncontrada) {
+
+            System.out.println("Localizada");
+        } else {
+
+            System.out.println("No localizada");
+        }
+        escaner.close();
+    }
+}
---
+++ Tema3/src/Actividades/H2Ejer2.java
@@ -0,0 +1,32 @@
+package Actividades;
+
+import java.util.Scanner;
+
+public class H2Ejer2 {
+    public static void main(String[] args) {
+
+        /*
+         * Crea una aplicación que vaya leyendo y guardando en un array las estaturas de
+         * 10 personas. A continuación, recorrerá el array y nos informará de si alguna de estas
+         * estaturas es mayor de 1'90 m.
+         */
+
+        Scanner escaner = new Scanner(System.in);
+        
+        int[] alturas = new int[10];
+
+        for (int i = 0; i<alturas.length; i++) {
+
+            System.out.println("Introduce la altura nº" + (i +1) + ", en centimetros:");
+            alturas[i] = escaner.nextInt();
+
+            if (alturas[i] > 190) {
+
+                System.out.println("La edad introducida es superior a 190cm");
+
+            }
+
+        }
+        escaner.close();
+    }
+}
