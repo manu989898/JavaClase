b3453c5be4d45680b6262b36fb2cf0eee114e167
manu989898 <apkmanu1@gmail.com>
2023-11-21 19:43:47 +0100

    ghj

--- Tema3/src/Ejemplos/MinYmaxArray.java
+++ Tema3/src/Ejemplos/MinYmaxArray.java
@@ -4,25 +4,32 @@ public class MinYmaxArray {
     public static void main(String[] args) {
         
         /*
-         * *** LOCALIZAR MÍNIMOS Y MAXIMOS EN UN ARRAY ***
+         * *** LOCALIZAR MÍNIMOS Y MÁXIMOS EN UN ARRAY ***
          * 
          * edades { [] [] [] [] [] [] }
          *     i    0  1  2  3  4  5
          * 
-         * posMin = 0;
+         * posMax = 1;
+         * posMin = 1;
          * for () para recorrer array
          * Si i < posMin le asignaremos ese valor a posMin 
          */
         
         int[] edades = {9,20,14,3,74,54};
-        int posMin = 0;
+        int posMin = 1;
+        int posMax = 1;
 
         for (int i = 1; i < edades.length;i++){
 
             if (edades[i] < edades[posMin]){
                 posMin = i;
             }
+            if (edades[i] > edades[posMax]) {
+                posMax = i;
+            }
         }
         System.out.println("El valor mínimo es: " + edades[posMin] + " Está en la posición: " + posMin);
+        System.out.println("El valor máximo es: " + edades[posMax] + " Está en la posición: " + posMax);
+
     }
 }
