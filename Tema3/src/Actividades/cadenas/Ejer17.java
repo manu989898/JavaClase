eba04390d03494323569a54428d244ea668c8397
manu989898 <apkmanu1@gmail.com>
2023-12-18 18:41:05 +0100

    sdfsd

---
+++ Tema3/src/Actividades/cadenas/Ejer17.java
@@ -0,0 +1,27 @@
+package Actividades.cadenas;
+
+import java.util.Scanner;
+
+public class Ejer17 {
+    public static void main(String[] args) {
+        /*
+         * Programa que lee una frase desde el teclado y la muestra quitando la última palabra.
+         */
+
+         Scanner escaner = new Scanner(System.in);
+        
+        System.out.println("Introduce una frase: ");
+        String frase = escaner.nextLine();
+
+        // Separamos la frase en palabras y las guardamos en un array.
+        String[] palabras = frase.split(" ");
+        String fraseSinUltimaPalabra = "";
+
+        // Recorremos el array de palabras hasta la penúltima.
+        for (int i = 0; i < palabras.length - 1; i++) {
+            fraseSinUltimaPalabra += palabras[i] + " ";
+        }
+        System.out.println("Frase sin la última palabra: " + fraseSinUltimaPalabra);
+        escaner.close();
+    }
+}
