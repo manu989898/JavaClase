b87d7dba8d0e6e8aa94e48a347b2915b6fdf97e7
manu989898 <apkmanu1@gmail.com>
2024-01-11 17:50:16 +0100

    kh

---
+++ Tema4/src/Actividades/Ejer10.java
@@ -0,0 +1,34 @@
+package Actividades;
+
+import java.util.Scanner;
+
+public class Ejer10 {
+    public static void main(String[] args) {
+
+        /*
+         * Escribe un método que escriba en pantalla el nombre de un mes a partir de su número. 
+         * En caso de que el número proporcionado no esté entre 1 y 12, deberá terminar inmediatamente.
+         */
+
+        String[] mes = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+
+        Scanner escaner = new Scanner(System.in);
+        System.out.println("Ingresa el tu mes favorito en forma numérica.");
+        int seleccion = escaner.nextInt() -1;
+        
+        System.out.println("El mes elegido es: ");
+        elegir(seleccion, mes);
+        escaner.close();
+    }
+
+
+
+
+    static void elegir(int seleccion, String[] mes ) {
+
+        if (seleccion>12 || seleccion<1) {
+            return;
+        }
+        System.out.println(mes[seleccion]);
+    }
+}
---
+++ Tema4/src/Actividades/Ejer9.java
@@ -0,0 +1,50 @@
+package Actividades;
+
+import java.util.Scanner;
+
+public class Ejer9 {
+    public static void main(String[] args) {
+
+        /*
+         * Crea un método int max(int x, int y) que devuelva el mayor de los números x e
+         * y. Análogamente,
+         * crea el método int min(int x, int y).
+         */
+
+        Scanner escaner = new Scanner(System.in);
+
+        System.out.println("Ingresa el primer número");
+        int num1 = escaner.nextInt();
+
+        System.out.println("Ingresa el segundo número");
+        int num2 = escaner.nextInt();
+
+        System.out.println("El número mayor es: ");
+        max(num1, num2);
+
+        System.out.println("El númeor menor es: ");
+        min(num1, num2);
+        escaner.close();
+    }
+
+    static void max(int num1, int num2) {
+
+        if (num1 < num2) {
+            
+            System.out.println(num2);
+        } else {
+            System.out.println(num1);
+        }
+
+    }
+
+    static void min(int num1, int num2) {
+
+        if (num1 > num2) {
+            
+            System.out.println(num2);
+        } else {
+            System.out.println(num1);
+        }
+    }
+}
