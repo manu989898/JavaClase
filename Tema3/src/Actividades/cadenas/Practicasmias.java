37152369042165df723f1b6cfc6f7b6e8b94b456
manu989898 <apkmanu1@gmail.com>
2024-01-17 16:37:58 +0100

    locos

--- Tema3/src/Actividades/cadenas/Practicasmias.java
+++ Tema3/src/Actividades/cadenas/Practicasmias.java
@@ -1,5 +1,6 @@
 package Actividades.cadenas;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Practicasmias {
@@ -37,6 +38,56 @@ public class Practicasmias {
                 System.out.print(frase1.charAt(i));
             }
         }
-        escaner.close();
+
+        // Ordenar arrays
+
+        int[] numeros = {5, 6, 1, 2, 9, 5, 4, 7};
+
+        int aux;
+        int posMin;
+
+        for (int i = 0; i < numeros.length-1; i++) {
+            posMin=i;
+
+            for (int j = i+1; j < numeros.length; j++) {
+                if (numeros[j] < numeros[posMin]) {
+                    posMin = j;
+                }
+            }
+            aux = numeros[posMin];
+            numeros[posMin] = numeros[i];
+            numeros[i] = aux;
+        }
+
+        System.out.println("\n"+Arrays.toString(numeros));
+        
+
+        // Ordenación de arrays bidimensionales.
+
+        String[][] sueldos = {{"Pedro", "1200"},{"Manu", "1500"},{"Luis", "1000"},{"Juan", "1700"} };
+
+        String[] auxx = new String[2];
+        int posMinn;
+
+        for (int i = 0; i < sueldos.length-1; i++) {
+            
+            posMinn=i;
+
+            for (int j = i +1; j < sueldos.length; j++) {
+                
+                if (Integer.parseInt(sueldos[j][1]) < Integer.parseInt(sueldos[posMinn][1])) {
+                    posMinn = j;
+                }
+            }
+            
+            auxx = sueldos[posMinn];
+            sueldos[posMinn] = sueldos[i];
+            sueldos[i] = auxx;
+            
+        }
+        System.out.println("\nArray ordenado por sueldos: ");
+        for (int i = 0; i < sueldos.length; i++) {
+            System.out.println(sueldos[i][0] + " " + sueldos[i][1]);
+        }
     }
 }
