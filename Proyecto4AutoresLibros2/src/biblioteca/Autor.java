d399dd654dd6e5497d3c7ab435dc8fef1278238c
manu989898 <apkmanu1@gmail.com>
2024-01-31 20:53:39 +0100

    cositas

---
+++ Proyecto4AutoresLibros2/src/biblioteca/Autor.java
@@ -0,0 +1,71 @@
+package biblioteca;
+
+public class Autor {
+
+	private String nombre;
+	private String email;
+	private int anioNacimiento;
+	
+	public Autor() {
+		
+	}
+
+	public Autor(String nombre, String email, int anioNacimiento) {
+		
+		this.nombre = nombre;
+		this.email = email;
+		this.anioNacimiento = anioNacimiento;
+	}
+
+	public Autor(String nombre) {
+		
+		this.nombre = nombre;
+		this.email = "Unknown";
+		this.anioNacimiento = 0;
+	}
+
+	public String toString() {
+		
+		return "Nombre: " + nombre + ", email: " + email + ", año nacimiento" + anioNacimiento;
+	}
+	
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public int getAnioNacimiento() {
+		return anioNacimiento;
+	}
+
+
+
+	public void setAnioNacimiento(int anioNacimiento) {
+		this.anioNacimiento = anioNacimiento;
+	}
+	
+	
+	
+	
+	
+}
---
+++ Proyecto4AutoresLibros2/src/inicio/Inicio.java
@@ -0,0 +1,55 @@
+package inicio;
+
+import java.util.Scanner;
+
+import biblioteca.Autor;
+import biblioteca.Libro;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+
+		Scanner escaner = new Scanner(System.in);
+	
+		
+		// Pedimos datos del Autor.
+		System.out.println("Introduce el nombre del Autor:");
+		String nombre = escaner.nextLine();
+		
+		System.out.println("Introduce el email del Autor:");
+		String email = escaner.nextLine();
+		
+		System.out.println("Introduce el año de nacimiento del Autor:");
+		int anioNacimiento = escaner.nextInt();
+		
+		// Creamos el objeto autor1 de tipo Autor.
+		Autor autor1 = new Autor(nombre, email, anioNacimiento);
+		
+		
+		//Pedimos datos del Libro
+		System.out.println("Introduce el Título del libro");
+		
+		// Ponemos un nextLine() para vaciar el buffer. Asi evitamos el salto de pregunta.
+		escaner.nextLine();
+		
+		String titulo = escaner.nextLine();
+		
+		System.out.println("Introduce el isbn del libro");
+		String isbn = escaner.nextLine();
+		
+		System.out.println("Introduce el año actual");
+		int anio = escaner.nextInt();
+		
+		// Creamos el objeto libro1 de tipo Libro.
+		Libro libro1 = new Libro(titulo, isbn, autor1);
+		Libro.setAnioActual(anio);
+		
+		// Mostramos los datos con el método toString()
+		System.out.println("\n**** DATOS DEL AUTOR ****");
+		System.out.println(autor1.toString());
+		System.out.println("\n*** DATOS DEL LIBRO ****");
+		System.out.println(libro1.toString() + ", Año actual: " + Libro.getAnioActual());
+		
+	}
+
+}
