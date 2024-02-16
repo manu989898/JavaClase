14049b04813a23c030687c634acd9b7dd013e6fc
manu989898 <apkmanu1@gmail.com>
2024-02-16 19:07:36 +0100

    cosis

--- proyecto8instituto/src/inicio/Inicio.java
+++ proyecto8instituto/src/inicio/Inicio.java
@@ -8,21 +8,20 @@ public class Inicio {
 
 	public static void main(String[] args) {
 		
-		
 		Grupo grupo1 = new Grupo(1, "ESO", 3, "B");
-		
 		
 		Alumno alumno1 = new Alumno(100, "Perez", "Luis", grupo1);
 		Alumno alumno2 = new Alumno(101, "Gonzalez", "Marcos", grupo1);
 		Alumno alumno3 = new Alumno(102, "Cuesta", "Pedro", grupo1);
 		Alumno alumno4 = new Alumno(103, "Lopez", "Javier", grupo1);
-		
+
 		Instituto insti = new Instituto();
+
 		insti.addAlumno(alumno1);
 		insti.addAlumno(alumno2);
 		insti.addAlumno(alumno3);
 		insti.addAlumno(alumno4);
-		
+
 		insti.delAlumno(50);
 		insti.muestraAlumnos();
 		
--- proyecto8instituto/src/instituto/Instituto.java
+++ proyecto8instituto/src/instituto/Instituto.java
@@ -55,5 +55,4 @@ public class Instituto {
 		}
 		return -1;
 	}
-
 }
