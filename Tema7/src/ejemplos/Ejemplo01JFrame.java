949afddecd5f95f7c60a452f9292470d23542e25
manu989898 <apkmanu1@gmail.com>
2024-02-27 18:58:00 +0100

    cosis

--- Tema7/src/ejemplos/Ejemplo01JFrame.java
+++ Tema7/src/ejemplos/Ejemplo01JFrame.java
@@ -1,49 +1,52 @@
 package ejemplos;
 
-import java.awt.Component;
-import java.awt.FlowLayout;
-import javax.swing.ButtonGroup;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
+import java.awt.*;
+import javax.swing.*;
 
 public class Ejemplo01JFrame extends JFrame {
 
 	public Ejemplo01JFrame() {
-		
+
 		// Damos valores a la ventana.
-		setSize(275, 100);
+		setSize(510, 110);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+
+		// Layout sirve para aplicarle un ordena los objetos del JFrame.
 		FlowLayout dis = new FlowLayout();
 		setLayout(dis);
-		
-		//Creamos botones
+
+		// Creamos botones
 		JButton btnPlay = new JButton("Play");
 		JButton btnStop = new JButton("Stop");
 		JButton btnPausa = new JButton("Pausa");
-		
-		//Creamos un texto
-		JLabel lblPagina = new JLabel("Dirección página web: ", JLabel.LEFT);
-		
-		//Creamos un campo a rellenar
-		JTextField txtPagina = new JTextField("Escribe aquí", 10);
-		
-		//Creamos un checkbox
-		JCheckBox chkCursoJava = new JCheckBox("Curso de Java", true);
-		
+		JButton btnBuscar = new JButton("Buscar");
+
+		// Creamos un texto
+		JLabel lblPagina = new JLabel("REPRODUCTOR DE MÚSICA", JLabel.LEFT);
+
+		// Creamos un campo a rellenar
+		JTextField txtPagina = new JTextField("Buscar", 10);
+
+		// Creamos un checkbox
+		JCheckBox chkCursoJava = new JCheckBox("Like", false);
+
 		ButtonGroup cursos = new ButtonGroup();
-		
+
+		add(lblPagina);
 		add(btnPlay);
 		add(btnStop);
 		add(btnPausa);
 		add(txtPagina);
-		add(lblPagina);
-		setVisible(true);
+		add(btnBuscar);
 		add(chkCursoJava);
-		
+		setVisible(true);
+
 	}
 
+	public static void main(String[] args) {
+
+		Ejemplo01JFrame ventana = new Ejemplo01JFrame();
+
+	}
 }
--- Tema7/src/ejemplos/Main.java
+++ Tema7/src/ejemplos/Main.java
@@ -2,12 +2,6 @@ package ejemplos;
 
 public class Main {
 
-	public static void main(String[] args) {
 
-		
-		Ejemplo01JFrame ventana = new Ejemplo01JFrame();
-		
-		
-	}
 
 }
