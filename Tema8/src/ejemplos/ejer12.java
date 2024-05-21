c71f623e07f1900d893862e0811de046d585336d
manu989898 <apkmanu1@gmail.com>
2024-05-21 18:40:30 +0200

    chore: Eliminar clase ejer11 y corregir errores en Libreta y Main

--- Tema8/src/ejemplos/ejer12.java
+++ Tema8/src/ejemplos/ejer12.java
@@ -1,25 +1,23 @@
 package ejemplos;
 
-public class ejer11 {
+public class ejer12 {
 
 	public static void main(String[] args) {
 
-		char[] nombre = { 'a', 'l', 'o','n' };
+		char[] nombre = { 'a', 'l', 'o', 'n' };
 		System.out.println(esPalidromo(nombre, 0, 2));
 	}
 
 	public static boolean esPalidromo(char[] frase, int inicio, int fin) {
-
+		// Caso base.
 		if (fin <= inicio) {
 			return true;
 		}
-		//recursivo
+		// Caso recursivous.
 		if (frase[inicio] == frase[fin]) {
-			
-			return esPalidromo(frase, inicio + 1, fin - 1);
-			
-		} else {
 
+			return esPalidromo(frase, inicio + 1, fin - 1);
+		} else {
 			return false;
 		}
 	}
